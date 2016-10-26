1b8565f1c88793bf183adc4ecdaa0e57f09b5517
charrondev <charrondev@gmail.com>
2016-10-25 22:05:02 -0400

    Delete built required folder

--- core/required/mime.js
+++
@@ -1,3 +0,0 @@
-module.exports = require('mime-types');
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1pbWUudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsTUFBTSxDQUFDLE9BQU8sR0FBRyxPQUFPLENBQUMsWUFBWSxDQUFDLENBQUMiLCJmaWxlIjoibWltZS5qcyIsInNvdXJjZXNDb250ZW50IjpbIm1vZHVsZS5leHBvcnRzID0gcmVxdWlyZSgnbWltZS10eXBlcycpO1xuIl19
